343e7e393d4f942eb9bdcdeece9e50e33ddc0450
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-23 09:55:54 +0200

    adding content to header and event handler

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,14 +2,38 @@ import classNames from 'classnames'
 import { useStyles } from './styles'
 import { useGlobalStyles } from '../../styles'
 import { Link } from 'react-router-dom'
-//import { useCallback } from 'react';
-import { IconButton } from '@mui/material';
+import { useCallback, useState } from 'react';
+import { Drawer, Hidden, IconButton } from '@mui/material';
 import { ReactComponent as Logo } from '../../assets/images/logo.svg'
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
     const classes = useStyles();
     const globalStyles = useGlobalStyles();
+
+    const [ openMenu, setOpenMenu ] = useState(false);
+
+    const listItemClickHandler = useCallback(() => setOpenMenu(false), []);
+
+    const getList = useCallback(() => (
+        <>
+            <li onClick={listItemClickHandler} className={classNames(classes.headerListItem)}>
+                <Link to="/portfolio" className={classNames(`uppercase`)}>
+                    Portfolio
+                </Link>
+            </li>
+            <li onClick={listItemClickHandler} className={classNames(classes.headerListItem)}>
+                <Link to="/about-us" className={classNames(`uppercase`)}>
+                    About us
+                </Link>
+            </li>
+            <li onClick={listItemClickHandler} className={classNames(classes.headerListItem)}>
+                <Link to="/contact" className={classNames(`uppercase`)}>
+                    Contact
+                </Link>
+            </li>
+        </>
+    ), [ classes, listItemClickHandler ]);
     
     return (
         <header className={classNames(globalStyles.px, `flex items-center justify-between py-3`)}>
@@ -17,9 +41,30 @@ const Header = () => {
                 <Logo />
             </Link>
             <div>
-                <IconButton>
-                    <MenuIcon />
-                </IconButton>
+                <Hidden smUp>
+                    <IconButton onClick={() => setOpenMenu(b => !b)}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Drawer
+                        anchor="right"
+                        open={openMenu}
+                        classes={{ paper: classes.drawerPaper, root: classes.drawerRoot }}
+                        onClose={() => setOpenMenu(false)}
+                        >
+                        <ul 
+                            className={classNames(classes.headerList, `flex flex-col`)}>
+                            { getList() }
+                        </ul>
+                    </Drawer>
+                </Hidden>
+                <Hidden smDown>
+                <nav className={classNames(``)}>
+                    <ul 
+                        className={classNames(classes.headerNavList, `flex items-center`)}>
+                        { getList() }
+                    </ul>
+                </nav>
+            </Hidden>
             </div>
         </header>
     )
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,45 @@
 import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles(theme => ({
-   
+    headerList: {
+        listStyleType: 'none',
+        padding: '1.8rem 0',
+        '& li:hover': {
+            backgroundColor: '#ebebeb45',
+            cursor: 'pointer'
+        },
+        '& a': {
+            color: '#FFF'
+        }
+    },
+    headerListItem: {
+        padding: '.7rem 24px',
+        [theme.breakpoints.up('sm')]: {
+            fontSize: '.83rem',
+            marginRight: '1.5rem',
+            padding: '.3rem 0',
+            '&:last-child': {
+                marginRight: 0
+            },
+            '& a': {
+                color: '#333136',
+                transition: 'all 1s ease-in-out',
+                '&:hover': {
+                    textDecoration: 'underline',
+                }
+            }
+        }
+    },
+    drawerRoot: {
+        top: '64px !important',
+        '& .MuiBackdrop-root': {
+            top: '64px !important'
+        }
+    },
+    drawerPaper: {
+        backgroundColor: '#979797 !important',
+        height: 'auto !important',
+        top: '64px !important',
+        width: '85%'
+    },
 }));
