e6d6a0c9e5d1de0e90012c4012ed6db869085a56
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-23 15:03:01 +0200

    adding colors

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -5,4 +5,40 @@ export const useGlobalStyles = makeStyles(theme => ({
         paddingLeft: '5%',
         paddingRight: '5%',
     },
+    brightRed: {
+        color: '#F43030'
+    },
+    brightRedBg: {
+        backgroundColor: '#F43030'
+    },
+    cyan: {
+        color: '#5FB4A2'
+    },
+    cyanBg: {
+        backgroundColor: '#5FB4A2'
+    },
+    darkBlue: {
+        color: '#203A4C'
+    },
+    darkBlueBg: {
+        backgroundColor: '#203A4C'
+    },
+    grayishDarkBlue: {
+        color: '#33323D'
+    },
+    grayishDarkBlueBg: {
+        backgroundColor: '#33323D'
+    },
+    lightGrey: {
+        color: '#EAEAEB'
+    },
+    lightGreyBg: {
+        backgroundColor: '#EAEAEB'
+    },
+    veryLightGrey: {
+        color: '#FAFAFA'
+    },
+    veryLightGreyBg: {
+        backgroundColor: '#FAFAFA'
+    }
 }));
