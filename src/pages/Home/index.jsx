d09406e030b10ea700cd873e1fedb820778b1879
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-24 10:20:16 +0200

    creating homepage hero section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,8 +10,30 @@ const Home = () => {
     const globalStyles = useGlobalStyles();
     
     return (
-        <main>
-            <h1 className={classNames('text-2xl')}>Hello</h1>
+        <main className={classNames(globalStyles.px)}>
+            <section>
+                <div 
+                    aria-label='illustration image'
+                    className={classNames(classes.heroIllustration, `bg-cover bg-no-repeat bg-center`)}>
+                </div>
+                <div className={classNames(`flex flex-col py-6`)}>
+                    <Typography 
+                        className={classNames(`font-bold text-blue-500 text-3xl`)}
+                        component="h1">
+                        Hey, I’m Alex Spencer and I love building beautiful websites
+                    </Typography>
+                    <Link 
+                        className={classNames(classes.heroLink, `uppercase text-slate-50 bg-blue-700 flex 
+                        items-center mt-6`)}
+                        to="/" >
+                        <span 
+                            aria-label='arrow icon'
+                            className={classNames(classes.heroLinkIcon, `bg-no-repeat bg-center`)}>
+                        </span>
+                        <span className={classNames(`grow text-center`)}>About me</span>
+                    </Link>
+                </div>
+            </section>
         </main>
     )
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,19 @@
 import { makeStyles } from '@mui/styles'
+import heroIcon from '../../assets/images/icons/down-arrows.svg'
+import heroIllustrationImage from '../../assets/images/homepage/mobile/image-homepage-hero.jpg'
 
 export const useStyles = makeStyles(theme => ({
-    
+    heroIllustration: {
+        backgroundImage: `url(${heroIllustrationImage})`,
+        height: 260
+    },
+    heroLink: {
+        width: 183
+    },
+    heroLinkIcon: {
+        backgroundImage: `url(${heroIcon})`,
+        backgroundSize: 25,
+        height: 45,
+        width: 45
+    }
 }))
